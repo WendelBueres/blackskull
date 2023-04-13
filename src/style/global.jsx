c7a2a4f01361a4301804f015fb574634bfdc44df
Wendel Bueres <wendelbueres@gmail.com>
2023-04-13 20:29:49 -0300

    feat: :sparkles: styleglobal

---
+++ src/style/global.jsx
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+:root{
+  //Palette colors
+    
+    //White
+    --white: #FFFFFF;
+
+    //Black
+    --black1: #0B0B0B;
+
+    //Oranges
+    --orange1: #FF9601;
+    --orange2: #FFC470;
+
+    //Darks
+    --dark1: #1C1C1E;
+    --dark2: #2C2C2E;
+    --dark3: #3A3A3C;
+
+
+    //Grays
+    --grey1: #9A9A9A;
+    --grey2: #CACACA;
+
+    //Background
+    --background: #F1F1F1;
+
+
+
+  // typogrady
+
+  --Heading-1-700: 700;
+  --Heading-2-600: 600;
+  --Heading-3-500: 500;
+
+  --body-1-400: 400;
+  --body-2-500: 500;
+
+  --input-placeholder: 400;
+  --input-label: 500;
+  --button-big-text: 600;
+  --button-medium-text: 600;
+
+  --heading: 'Lexend';
+  --body: 'Inter';
+  --button: 'Inter';
+  --input: 'Inter';
+
+
+
