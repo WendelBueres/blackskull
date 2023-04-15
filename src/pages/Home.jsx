f6a66188f5a9e5374b320449e76a93018bd2e1c7
Wendel Bueres <wendelbueres@gmail.com>
2023-04-15 01:49:27 -0300

    feat: :sparkles: adicionado componente Carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import BackToTop from "../components/BackToTop";
+import Carousel from "../components/Carousel";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Carousel />
       <BackToTop />
     </>
   );
