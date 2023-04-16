7788574bb05cc50e616908c364f3551b72271082
Wendel Bueres <wendelbueres@gmail.com>
2023-04-16 17:58:18 -0300

    feat: :sparkles: renderizado texto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { Typography } from "@mui/material";
 import BackToTop from "../components/BackToTop";
 import Carousel from "../components/Carousel";
 import CarouselCategory from "../components/CarouselCategory";
 import CarouselProduct from "../components/CarouselProducts";
 import NavBar from "../components/NavBar";
+import { products } from "../source-data";
 
 export default function Home() {
   return (
@@ -10,7 +12,21 @@ export default function Home() {
       <NavBar />
       <Carousel />
       <CarouselCategory />
-      <CarouselProduct />
+      <Typography
+        sx={{
+          fontFamily: "Roboto Condensed, sans-serif",
+          fontStyle: "italic",
+          fontWeight: 700,
+          fontSize: "32px",
+          lineHeight: "37.5px",
+          textAlign: "center",
+          mt: "64px",
+          mb: "24px",
+        }}
+      >
+        LANÇAMENTOS
+      </Typography>
+      <CarouselProduct products={products} />
       <BackToTop />
     </>
   );
