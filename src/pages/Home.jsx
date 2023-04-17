5e0a0548f02fa29584ab5f0b9bc76f965c6fb265
Wendel Bueres <wendelbueres@gmail.com>
2023-04-16 22:35:07 -0300

    feat: :sparkles: renderizado SectionTroopSkull

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import CarouselProduct from "../components/CarouselProducts";
 import NavBar from "../components/NavBar";
 import { products } from "../source-data";
 import SectionBlog from "../components/SectionBlog";
+import SectionTroopSkull from "../components/SectionTroopSkull";
 
 export default function Home() {
   return (
@@ -26,6 +27,22 @@ export default function Home() {
         }}
       >
         LANÇAMENTOS
+      </Typography>
+      <CarouselProduct products={products} />
+      <SectionTroopSkull />
+      <Typography
+        sx={{
+          fontFamily: "Roboto Condensed, sans-serif",
+          fontStyle: "italic",
+          fontWeight: 700,
+          fontSize: "32px",
+          lineHeight: "37.5px",
+          textAlign: "center",
+          mt: "64px",
+          mb: "24px",
+        }}
+      >
+        PROMOÇÕES
       </Typography>
       <CarouselProduct products={products} />
       <SectionBlog />
